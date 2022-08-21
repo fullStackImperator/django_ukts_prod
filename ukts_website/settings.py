890d709af8fe519494db7aa8a16384ee2c2bb7c7
fullStackImperator <stevanv@gmx.de>
2022-08-21 04:10:24 +0200

    S3 added

--- ukts_website/settings.py
+++ ukts_website/settings.py
@@ -56,6 +56,8 @@ INSTALLED_APPS = [
     'zatrenere.apps.ZatrenereConfig',
     'phonenumber_field',
     'django_countries',
+
+    'storages'
 ]
 
 
@@ -183,18 +185,27 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # VENV_PATH = os.path.dirname(BASE_DIR)
-# STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
 
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR /"media/"
 
 
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+# S3 configuration
+AWS_QUERYSTRING_AUTH = False
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AWS_S3_ACCESS_KEY_ID = 'AKIAW5FRUHHA72EAPMOY'
+AWS_S3_SECRET_ACCESS_KEY = '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'
+
+AWS_STORAGE_BUCKET_NAME = 'ukts-demo'
 
 
 #SMTP Configuration
