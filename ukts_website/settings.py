7c5e0e701bf81ff915fd4d2b740824e04c2a866b
fullStackImperator <stevanv@gmx.de>
2022-08-22 04:27:03 +0200

    login prob

--- ukts_website/settings.py
+++ ukts_website/settings.py
@@ -148,7 +148,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 LOGIN_REDIRECT_URL = 'treneri:treneri_list' 
-LOGIN_URL = 'login'
+LOGIN_URL = 'login:login'
 # LOGOUT_URL = 'post_list'
 LOGOUT_REDIRECT_URL = 'blog:post_list'
 
@@ -191,18 +191,18 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR /"media/"
 
 
-# # S3 configuration
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# S3 configuration
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-# AWS_QUERYSTRING_AUTH = False
-# AWS_DEFAULT_ACL = 'public-read'
+AWS_QUERYSTRING_AUTH = False
+AWS_DEFAULT_ACL = 'public-read'
 
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AWS_S3_ACCESS_KEY_ID = 'AKIAW5FRUHHA72EAPMOY'
-# AWS_S3_SECRET_ACCESS_KEY = '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'
+AWS_S3_ACCESS_KEY_ID = 'AKIAW5FRUHHA72EAPMOY'
+AWS_S3_SECRET_ACCESS_KEY = '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'
 
-# AWS_STORAGE_BUCKET_NAME = 'ukts-demo'
+AWS_STORAGE_BUCKET_NAME = 'ukts-demo'
 
 
 
